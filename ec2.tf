87bfc089d99fe2e4872233d661e2fc7b2e2b61c8
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2023-04-06 19:11:13 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+# AWS EC2 Instance Terraform Module
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.17.0"
+  # insert the 10 required variables here
+  name                   = "my-ec2"
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.public_sg.this_security_group_id]
+
+}
+
+
+# Resource - depends_on Meta-Argument
+resource "aws_eip" "bastion_eip" {
+  depends_on = [ module.ec2_public, module.vpc ]
+  instance = module.ec2_public.id[0]
+  vpc      = true
+}
