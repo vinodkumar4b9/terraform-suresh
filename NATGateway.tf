ed61423b2360f8f052f7f37792e34862411f1318
santhosh-goduri <126750881+santhosh-goduri@users.noreply.github.com>
2023-04-07 10:55:21 +0530

    Create NATGateway.tf

---
+++ NATGateway.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.example.id
+  subnet_id     = aws_subnet.example.id
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+}
